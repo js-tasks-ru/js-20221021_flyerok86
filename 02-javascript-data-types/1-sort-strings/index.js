880faaa8d03292c60af4e6db4020acb7f4078604
Pavel S <21025468+PASHANSX@users.noreply.github.com>
2022-10-27 18:13:51 +0300

    1-sort-strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    if(param === 'asc'){
+        return [...arr].sort( (p1, p2) => p1.localeCompare(p2, ['ru', 'en'], { caseFirst: 'upper' }) );
+    }
 
+    if(param === 'desc') {
+        return [...arr].sort( (p1, p2) => p2.localeCompare(p1, ['ru', 'en'], { caseFirst: 'upper' }) );
+    }
 }
